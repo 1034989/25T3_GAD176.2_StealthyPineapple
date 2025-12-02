5549f24ff82f0c4973ff9eb0fad14a71718f5c67
SAE - Lab 102 Back- Robert andren <1035695@student.sae.edu.au>
2025-12-02 16:19:22 +1100

    game Pauses when in Inventory
    when in inventory game should "pause" due to time.timeScale = 0 this is also reverted back when not in inventory as it sets timescale back to 1

--- Assets/inventorySystem/scripts/InventoryManager.cs
+++ Assets/inventorySystem/scripts/InventoryManager.cs
@@ -17,6 +17,16 @@ namespace SteathyPineapple.InventorySystem
             {
                 menuActivated = !menuActivated;
                 inventoryMenu.SetActive(menuActivated);
+                if (menuActivated == true)
+                {
+                    Time.timeScale = 0.0f;
+                    Debug.Log("timeScale is: " + Time.timeScale + " this means the game is paused when in inventory");
+                }
+                else
+                {
+                    Time.timeScale = 1.0f;
+                    Debug.Log("timeScale is: " + Time.timeScale + " this means the game is unpaused when in game");
+                }
             }
         }
         public int AddItem(string itemName, int quantity, string itemDiscription,int maxQuantity)
