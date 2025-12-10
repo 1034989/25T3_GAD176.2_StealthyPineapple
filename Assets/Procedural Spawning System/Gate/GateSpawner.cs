0114078f71186a77e400a6069d772391452f6c74
SohailDatt_Laptop <161532141+SohailDatt@users.noreply.github.com>
2025-12-11 04:00:41 +1100

    created gate script and spawning
    - gate script works with keycards however the gates dont "disappear"
    to do list:
    - merge with develop
    - create speed power up for physics movement

---
+++ Assets/Procedural Spawning System/Gate/GateSpawner.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using Keycards;
+
+public class GateSpawner : MonoBehaviour
+{
+    // Reference to the Gate prefab
+    public GameObject gatePrefab;
+
+    // Position where the gate will be spawned
+    public Transform gateSpawnPoint;
+
+    void Start()
+    {
+        SpawnGate();
+        
+    }
+
+    // Function to instantiate the gate prefab
+    private void SpawnGate()
+    {
+        if (gatePrefab != null && gateSpawnPoint != null)
+        {
+            Instantiate(gatePrefab, gateSpawnPoint.position, gateSpawnPoint.rotation);
+            Debug.Log("Gate spawned successfully!");
+        }
+    }
+}
--- Assets/Procedural Spawning System/Keycards/Script/Keycard.cs
+++ Assets/Procedural Spawning System/Keycards/Script/Keycard.cs
@@ -9,6 +9,8 @@ public class Keycard : KeycardParent
     [SerializeField]
     public string keycardID;
 
+    public GameObject Gate;
+
     // Method called when the keycard is picked up
     public void PickUp()
     {
@@ -28,8 +30,12 @@ public class Keycard : KeycardParent
         Debug.Log("Collected Keycard: " + keycardID);
         // Destroy keycard object after pickup
         Destroy(gameObject);
-
+        DisableGate();
     }
 
-
+    public void DisableGate()
+    {
+        Gate.SetActive(false);
+        Debug.Log("the gate has disappeared!!");
+    }
 }
